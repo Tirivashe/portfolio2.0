d64943fe042135ccea80093ff2d016d7b1dba44c
Tiri <shaymusts@gmail.com>
2021-10-23 19:15:41 +0200

    Added theming to dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from 'next-themes'
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
 import '../styles/globals.css'
@@ -5,15 +6,17 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="grid grid-cols-12 gap-6 px-5 lg:px-48 my-14 sm:px-20 md:px-32">
-      <div className="col-span-12 p-4 text-center bg-white lg:col-span-4 rounded-2xl">
-        <Sidebar />
+    <ThemeProvider attribute='class'>
+      <div className="grid grid-cols-12 gap-6 px-5 lg:px-48 my-14 sm:px-20 md:px-32">
+        <div className="col-span-12 p-4 text-center bg-white dark:bg-dark-500 lg:col-span-3 rounded-2xl shadow-custom-light dark:shadow-custom-dark">
+          <Sidebar />
+        </div>
+        <div className="flex flex-col col-span-12 overflow-hidden bg-white shadow-custom-light dark:shadow-custom-dark lg:col-span-9 rounded-2xl dark:bg-dark-500">
+          <Navbar />
+          <Component {...pageProps} />
+        </div>
       </div>
-      <div className="flex flex-col col-span-12 bg-white lg:col-span-8 rounded-2xl">
-        <Navbar />
-        <Component {...pageProps} />
-      </div>
-    </div>
+    </ThemeProvider>
   )
   
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,13 +4,13 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang='en'>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com"/>
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true'/>
           <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet" />
         </Head>
-        <body className='bg-gradient-to-r from-green to-blue-400'>
+        <body className='bg-gradient-to-r from-green to-blue-400 dark:from-dark-500 dark:to-dark-700 dark:text-white'>
           <Main />
           <NextScript />
         </body>
