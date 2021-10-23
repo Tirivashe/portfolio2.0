4631bfeaa788e06c18fa02dbf5f93de40a4039bc
Tiri <shaymusts@gmail.com>
2021-10-23 19:14:17 +0200

    created types for services and skills

---
+++ types.ts
@@ -0,0 +1,13 @@
+import { IconType } from "react-icons";
+
+export interface IService{
+  title: string,
+  details: string,
+  Icon: IconType
+}
+
+export interface ISkill{
+  name: string,
+  level: string,
+  Icon: IconType
+}
