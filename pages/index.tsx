af3143a19910a2a1ef77f372bd5421b473f06486
Tiri <shaymusts@gmail.com>
2021-10-23 19:18:22 +0200

    Theming and rendering full home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,23 @@
-import React, { ReactElement } from 'react'
+import React from 'react'
+import ServiceCard from '../components/ServiceCard'
+import { services } from '../data'
 
-
-function index(): ReactElement {
+function index() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className='flex flex-col flex-grow px-6 pt-1'>
+      <h5 className='my-3 font-medium'>Software engineer experienced in object-oriented programming, web and mobile development, developing using APIs and full stack development</h5>
+      <div className='flex-grow p-4 mt-5 bg-gray-400 dark:bg-dark-100' style={{ marginLeft: '-1.5rem', marginRight: '-1.5rem' }}>
+        <h6 className='my-3 text-lg font-bold tracking-wide'>What I Offer</h6>
+        <div className='grid gap-6 lg:grid-cols-2'>
+
+          { services.map(service => (
+            <div key={Math.random()} className="bg-gray-200 rounded-lg dark:bg-dark-200 lg:col-span-1"> 
+              <ServiceCard service={service}/>
+            </div>
+          )) }
+          
+        </div>
+      </div>
     </div>
   )
 }
