8f1dafcb23d1456cd9a4e2276547ec7adc8cfef4
Tirivashe <shaymusts@gmail.com>
2024-03-21 13:28:51 +0200

    under construction loader

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,20 @@
+import styles from "./page.module.css";
+
 export default function Home() {
-  return <main>Clean start</main>;
+  return (
+    <main
+      style={{
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div className={styles.loaderContainer}>
+        <span className={styles.loader} />
+        <h3>Under Construction...</h3>
+        <span className={styles.loader2} />
+      </div>
+    </main>
+  );
 }
