a261f854af932e12f7c43a8e265b523ba1978cc5
Tirivashe <shaymusts@gmail.com>
2024-03-22 13:07:57 +0200

    clean slate

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,3 @@
-import styles from "./page.module.css";
-
 export default function Home() {
-  return (
-    <main
-      style={{
-        height: "100%",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <div className={styles.loaderContainer}>
-        <span className={styles.loader} />
-        <h3>Under Construction...</h3>
-        <span className={styles.loader2} />
-      </div>
-    </main>
-  );
+  return <main>Hello world</main>;
 }
