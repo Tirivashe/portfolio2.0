fa3564d6bd0fcefd27066323adef5cb056306018
Tirivashe <shaymusts@gmail.com>
2024-03-26 20:29:12 +0200

    added styles prop

--- app/_components/Loader/index.tsx
+++ app/_components/Loader/index.tsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 import styles from "./styles.module.scss";
 import { motion } from "framer-motion";
 
-const Loader = () => {
+type Props = {
+  styles?: CSSProperties;
+};
+
+const Loader = ({ styles: loaderStyles = {} }: Props) => {
   return (
     <motion.div
       className={styles.loader}
+      style={{ ...loaderStyles }}
       initial={{ opacity: 0, scale: 0 }}
       animate={{ opacity: 1, scale: 1 }}
       transition={{ duration: 0.5 }}
     />
   );
 };
-
 export default Loader;
