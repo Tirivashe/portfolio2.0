6e29d0d0abb745bc889b8ed58491c400ca1ee3dc
Tirivashe <shaymusts@gmail.com>
2024-03-27 17:23:31 +0200

    repositioned the glow and particles

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 "use client";
 
 import Header from "./_components/Header";
+import ParticlesComponent from "./_components/Particles";
 import About from "./_sections/About";
 import Hero from "./_sections/Hero";
 import Intro from "./_sections/Intro";
+import styles from "./styles.module.scss";
 
 export default function Home() {
   return (
-    <main style={{ position: "relative", overflowX: "hidden" }}>
+    <main style={{ position: "relative", overflow: "hidden" }}>
+      <ParticlesComponent />
+      <div className={`${styles.glow} ${styles.glow1}`} />
+      <div className={`${styles.glow} ${styles.glow2}`} />
       <Header />
       <Intro />
       <Hero />
