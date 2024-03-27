dbf49f0f1c49c2757a14af04886257e4050c2e98
Tirivashe <shaymusts@gmail.com>
2024-03-27 20:06:01 +0200

    added smooth scrolling

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,24 @@
 "use client";
 
+import { useEffect } from "react";
 import Header from "./_components/Header";
 import ParticlesComponent from "./_components/Particles";
 import About from "./_sections/About";
 import Hero from "./_sections/Hero";
 import Intro from "./_sections/Intro";
 import styles from "./styles.module.scss";
+import Lenis from "@studio-freight/lenis";
 
 export default function Home() {
+  useEffect(() => {
+    const lenis = new Lenis();
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <main style={{ position: "relative", overflow: "hidden" }}>
       <ParticlesComponent />
