91936ba718c315deac4ad23763f3567ffefd994e
Tirivashe <shaymusts@gmail.com>
2024-03-27 22:24:07 +0200

    added more props for marquee

--- app/_components/InfiniteScrollingText/index.tsx
+++ app/_components/InfiniteScrollingText/index.tsx
@@ -1,19 +1,37 @@
 import React from "react";
 import styles from "./styles.module.scss";
 import Marquee from "react-fast-marquee";
-import { Text } from "@mantine/core";
+import { MantineSize, Text } from "@mantine/core";
 import { IconComponents } from "@tabler/icons-react";
 
 type Props = {
   text: string;
+  textSize?: (string & {}) | MantineSize | undefined;
+  iconSize?: string;
+  color?: string | undefined;
 };
 
-const InfiniteScrollingText = ({ text }: Props) => {
+const InfiniteScrollingText = ({
+  text,
+  textSize = "md",
+  iconSize = "3rem",
+  color = "white",
+}: Props) => {
   return (
-    <Marquee autoFill>
-      <Text className={styles.text}>{text}</Text>
+    <Marquee autoFill style={{ overflow: "hidden" }}>
+      <Text
+        className={styles.text}
+        size={textSize}
+        style={{
+          WebkitTextStroke: "1px",
+          WebkitTextStrokeColor: color,
+        }}
+      >
+        {text}
+      </Text>
       <IconComponents
-        size="3rem"
+        size={iconSize}
+        color={color}
         style={{ margin: "0 30px", opacity: "0.5" }}
       />
     </Marquee>
