28768eb2d7cf7038ea62cb2e8ac182fbcb83dd1c
Tirivashe <shaymusts@gmail.com>
2024-03-28 17:34:49 +0200

    added technical skills and summary

--- app/_constants/index.ts
+++ app/_constants/index.ts
@@ -3,8 +3,61 @@ import {
   IconBrandInstagram,
   IconBrandLinkedin,
   IconBrandWhatsapp,
-  IconBrandX,
 } from "@tabler/icons-react";
+
+//TODO: Add more skills
+export const technicalSkills = [
+  {
+    id: 2,
+    name: "TypeScript",
+    icon: "/icon/typescript.svg",
+  },
+  {
+    id: 7,
+    name: "Node.js",
+    icon: "/icon/node-js.svg",
+  },
+  {
+    id: 3,
+    name: "React",
+    icon: "/icon/react.svg",
+  },
+  {
+    id: 4,
+    name: "Express js",
+    icon: "/icon/express.svg",
+  },
+  {
+    id: 8,
+    name: "MongoDB",
+    icon: "/icon/mongodb.svg",
+  },
+  {
+    id: 5,
+    name: "Golang",
+    icon: "/icon/go.svg",
+  },
+  {
+    id: 1,
+    name: "JavaScript",
+    icon: "/icon/javascript.svg",
+  },
+  {
+    id: 9,
+    name: "PostgreSQL",
+    icon: "/icon/postgresql.svg",
+  },
+  {
+    id: 10,
+    name: "Kotlin",
+    icon: "/icon/kotlin.svg",
+  },
+  {
+    id: 11,
+    name: "CSS",
+    icon: "/icon/css.svg",
+  },
+];
 
 export const socials = [
   {
@@ -28,3 +81,6 @@ export const socials = [
     icon: IconBrandWhatsapp,
   },
 ];
+
+export const summary =
+  "Results-oriented software developer with expertise in React and strong focus in frontend development. I elevate user engagement and deliver reliable web and mobile applications. I bring a strong technical foundation and essential skillset. A constant learner and eager to leverage my skills to contribute to your success.";
