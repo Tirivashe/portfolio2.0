b254b0f3debb5c9574c797a38c581d70a4dfe7ea
Tirivashe <shaymusts@gmail.com>
2024-03-28 19:08:15 +0200

    custom button component

---
+++ app/_components/CustomButton/index.tsx
@@ -0,0 +1,39 @@
+import { Button, ButtonProps } from "@mantine/core";
+import React from "react";
+import styles from "./styles.module.scss";
+import { motion } from "framer-motion";
+
+type Props = ButtonProps & {
+  text: string;
+};
+
+const CustomButton = ({ text, ...rest }: Props) => {
+  return (
+    <Button
+      component={motion.button}
+      {...rest}
+      style={{ backgroundColor: "var(--mantine-color-gray-9)" }}
+      classNames={{
+        root: styles.root,
+        label: styles.label,
+        inner: styles.inner,
+      }}
+      whileHover={{
+        backgroundColor: "#64bbcd",
+        scale: 1.1,
+        transition: {
+          duration: 0.1,
+          ease: "easeInOut",
+        },
+      }}
+      whileTap={{
+        scale: 0.9,
+      }}
+      transition={{ type: "spring", stiffness: 400, damping: 10 }}
+    >
+      {text}
+    </Button>
+  );
+};
+
+export default CustomButton;
